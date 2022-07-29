2d3128a0ff5689269419102a0a9690dcea7bf062
John <jooohn.eth@gmail.com>
2022-07-29 12:04:58 -0400

    Declaring variables and setting the owner

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -7,8 +7,17 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFTContract is ERC721, ERC721URIStorage{
 
+    using Counters for Counters.Counter;
+    Counters.Counter private tokedIds;
+
+    uint private constant MAX_SUPPLY = 100;
+    uint private constant MINT_PRICE = 0.01 ether;
+    uint private constant MAX_AMOUNT_PER_TRANSACTION = 5;
+
+    address private owner;
+
     constructor() ERC721("MyNFT", "MNFT"){
-        
+        owner = msg.sender;
     }
 
     //These functions need to be overridden for Solidity
