f4e1eac2fda79efd44a402478002b7f5b7ac03c1
John <jooohn.eth@gmail.com>
2022-07-29 12:00:17 -0400

    importing

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -2,9 +2,11 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTContract is ERC721{
-
+contract NFTContract is ERC721, ERC721URIStorage{
+    
     constructor() ERC721("MyNFT", "MNFT"){
         
     }
