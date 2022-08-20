bdb07d674842974f84f4a9aefdc79ebc6239ac79
John <jooohn.eth@gmail.com>
2022-08-20 14:57:27 -0400

    Added Natspec comments and modified redundant code

--- contracts/NFTContract.sol
+++ contracts/NFTContract.sol
@@ -1,77 +1,71 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-//importing openzeppeling libraries
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+///@notice Importing ERC721 standard from Openzeppelin with URI storage functionality
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFTContract is ERC721, ERC721URIStorage{
+///@notice Importing Ownable from Openzeppelin
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    //Creating counter to keep track of the number of NFTs minted
+///@notice Import a Counter contract from Openzeppelin
+import "openzeppelin/contracts/utils/Counters.sol";
+
+///@title ERC721 contract
+///@author John Nguyen | jooohn.eth
+///@notice An NFT contract of 100 NFTs
+///@dev Tested using Foundry, see test/NFTContract.t.sol
+contract NFTContract is ERC721URIStorage, Ownable{
+    
+    ///@notice Number of NFTs in a collection
     using Counters for Counters.Counter;
-    Counters.Counter private tokenIds;
+    Counters.Counter public tokenIds;
 
-    //Declaring constant variables
-    uint private constant MAX_SUPPLY = 100;
-    uint private constant MINT_PRICE = 0.01 ether;
-    uint private constant MAX_AMOUNT_PER_TRANSACTION = 5;
-    string private constant TOKEN_URI = "ipfs://QmYEhCEARig7Ur1Qkf5YwJtTZmfjTyXWbcDTzi5axqjLaU";
+    ///@notice Max Supply of Collection
+    uint public constant MAX_SUPPLY = 100;
 
-    address public owner;
+    ///@notice Mint price of one NFT in ETH
+    uint public constant MINT_PRICE = 0.01 ether;
 
-    //Setting the name and symbol of the NFT and setting the owner on contract deployment
-    constructor() ERC721("King Street Analytics", "KSA"){
-        owner = msg.sender;
-    }
+    ///@notice Number of NFTs a user can buy in one transaction
+    uint public constant MAX_AMOUNT_PER_TRANSACTION = 5;
 
-    //Mint function
+    string private constant TOKEN_URI = "ipfs://QmYEhCEARig7Ur1Qkf5YwJtTZmfjTyXWbcDTzi5axqjLaU"; //token uri, metadata uploaded to ipfs using pinata
+
+    ///@notice Name and Sybmbol of the collection
+    ///@dev The owner is set to deployer address, according to Ownable contract
+    constructor() ERC721("King Street Analytics", "KSA"){}
+
+    ///@notice Function to buy NFTs
+    ///@param amount Amount of NFTs a user wants to mint in a single transactio
+    ///@dev _safeMint,_setToTokenURI inherited from ERC721URIStorage contract
     function mint(uint amount) public payable {
-        //Basic requirnments for mint to execute
+
         require(tokenIds.current() < MAX_SUPPLY, "No NFTs left!");
         require(amount <= MAX_AMOUNT_PER_TRANSACTION, "Not allowed to mint more than 5 NFTs per transaction!");
         require(msg.value == MINT_PRICE * amount, "Not enough ETH!");
 
-        //For loop to mint multiple amounts of NFTs provided by the user
         for(uint i = 0; i < amount; i++){
 
             uint tokenID = tokenIds.current();
             tokenIds.increment();
-            //_safeMint function inherited from ERC721 contract
+
             _safeMint(msg.sender, tokenID);
-            //Setting token URI for each minted NFT
             _setTokenURI(tokenID, TOKEN_URI);
 
         }
     }
 
-    //Method overloading, called when minting one NFT
+    ///@dev Method overloading implemented
     function mint() external payable {
         mint(1);
     }
 
-    //Withdraw function, sends the owner of the contract the amount of money collected from the mints
-    function withdraw() external {
-        //Only lets the owner to execute the function
-        require(msg.sender == owner, "Not owner!");
+    ///@notice Function that lets the owner of the contract to receive funds
+    function withdraw() external onlyOwner{
         
         (bool success, ) = (msg.sender).call{value: address(this).balance}("");
         require(success, "Tx failed!");
-    }
-
-    //These functions need to be overridden for Solidity
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
+        
     }
 
 }
